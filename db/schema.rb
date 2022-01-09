707c44e9b2971ef4a811a808b0720558cd9cfb6a
Thiago Costa <thiagohrcosta86@gmail.com>
2022-01-09 13:33:35 -0300

    Adds status do products table and adds tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_170831) do
+ActiveRecord::Schema.define(version: 2022_01_09_163051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2021_12_21_170831) do
     t.bigint "productable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable"
   end
 
